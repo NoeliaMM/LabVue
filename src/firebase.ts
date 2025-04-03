e8d056b9e70eff654ec8ba4969f355c11212f882
NoeMM <mm.noe2011@gmail.com>
2025-04-03 21:25:29 +0200

    :(

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,24 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 // 🔥 Configuración de Firebase (usa tus propias credenciales)
 const firebaseConfig = {
-  apiKey: "AIzaSyDHIiY5a_tYko76PnDhnfhAD8kdppyMdZ4",
-  authDomain: "todolist-vue-b992b.firebaseapp.com",
-  projectId: "todolist-vue-b992b",
-  storageBucket: "todolist-vue-b992b.firebasestorage.app",
-  messagingSenderId: "761761536328",
-  appId: "1:761761536328:web:251965cd0d65ab5d337b7d"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 // Inicializar Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
 // Configuración de servicios
-const db = getFirestore(firebaseApp); // Base de datos
+const db = getFirestore(firebaseApp) // Base de datos
 
-
-export { db };
-
-
-
+export { db }
